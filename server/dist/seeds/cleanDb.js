2940dec62841659475dc6a42099245fbb4dfb38d
Jonathan Hummer <jonathan.hummer@gmail.com>
2025-05-18 06:17:02 -0400

    gd garden lanterns

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -2,6 +2,9 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
+        if (!models[modelName].db?.db) {
+            throw new Error(`Database connection for model "${modelName}" is not available.`);
+        }
         let modelExists = await models[modelName].db.db.listCollections({
             name: collectionName
         }).toArray();
