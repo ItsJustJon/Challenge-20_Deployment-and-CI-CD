0924cf75ce1008a0da6be2e3b7b3aaa1bc89b186
Jonathan Hummer <jonathan.hummer@gmail.com>
2025-05-18 06:07:24 -0400

    Undefined seeds

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,6 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if (!models[modelName].db?.db) {
+      throw new Error(`Database connection for model "${modelName}" is not available.`);
+    }
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
